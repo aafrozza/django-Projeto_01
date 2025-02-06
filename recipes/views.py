de06cff6e9aceaab05647675d419ed238ad6b13b
Angelo Augusto Frozza <frozza@gmail.com>
2025-02-06 14:08:12 -0300

    Aula26

---
+++ recipes/views.py
@@ -0,0 +1,16 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+
+
+def home(request):
+    return render(request, 'recipes/home.html', context={
+    'name': 'Luiz Otávio viws',
+})
+
+
+def contato(request):
+    return render(request, 'recipes/contato.html')
+
+
+def sobre(request):
+    return HttpResponse('sobre')
