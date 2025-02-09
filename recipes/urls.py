75ebbee998c02c57544f22313ac7fb557430de23
Angelo Augusto Frozza <frozza@gmail.com>
2025-02-08 21:10:40 -0300

    Aula 50

--- recipes/urls.py
+++ recipes/urls.py
@@ -2,9 +2,16 @@ from django.urls import path
 #from recipes.views import contato, home, sobre
 from . import views
 
+app_name = 'recipes'
+
+
 urlpatterns = [
-    path('', views.home),  # Home
+    #path('', views.home),  # Home
+    path('', views.home, name="home"),
+ 
     #path('recipes/', views.home),  # Home
     path('recipes/<int:id>/', views.recipe),  # /recipes/3/
     #path('contato/', contato),  # /contato/
+    path('recipes/<int:id>/', views.recipe, name="recipe"),
+
 ]
