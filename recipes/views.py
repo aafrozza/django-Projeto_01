6af2f7b47071de31a5d092818f19e1315b850bfc
Angelo Augusto Frozza <frozza@gmail.com>
2025-02-09 18:16:41 -0300

    Aula 63

--- recipes/views.py
+++ recipes/views.py
@@ -1,11 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import get_list_or_404, render
 from utils.recipes.factory import make_recipe
-from django.http import Http404
 
 
 from .models import Recipe
-
-
 
 def home(request):
     recipes = Recipe.objects.filter(
@@ -16,27 +13,28 @@ def home(request):
 })
 
 def category(request, category_id):
-    recipes = Recipe.objects.filter(
-        category__id=category_id,
-        is_published=True,
-    ).order_by('-id')
-    
-    if not recipes:
-        raise Http404('Not found 🥲')
+    recipes = get_list_or_404(
+        Recipe.objects.filter(
+            category__id=category_id,
+            is_published=True,
+        ).order_by('-id')
+    )
 
     return render(request, 'recipes/pages/category.html', context={
         'recipes': recipes,
-        'title': f'{recipes.first().category.name} - Category | '
+        'title': f'{recipes[0].category.name} - Category | '
  })
 
 
 def recipe(request, id):
-    recipe_1 = Recipe.objects.filter(
-        id = id
-    )
+    recipe = Recipe.objects.filter(
+        pk=id,
+        is_published=True,
+    ).order_by('-id').first()
+       
     return render(request, 'recipes/pages/recipe-view.html', context={
         #'recipe': make_recipe(),
-        'recipe': recipe_1,
+        'recipe': recipe,
         'is_detail_page': True,
     })
 
